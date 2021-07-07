a6c0b26eb83ec04ca970c5f0cc872b1b17781385
AMihaylenko <amikhaylenko@metaprime.ru>
2021-07-07 16:20:58 +0300

    db cron job

---
+++ cronJob.js
@@ -0,0 +1,37 @@
+const { Sequelize, Op  } = require('sequelize');
+require('dotenv').config();
+const sevenDaysAgo = new Date(new Date().setDate(new Date().getDate() - 7));
+// const sequelize = new Sequelize(`${process.env.DATABASE_URI}?sslmode=require`, {
+const sequelize = new Sequelize(`${process.env.DATABASE_URI}?sslmode=require`, {
+  url: process.env.DATABASE_URI,
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // very important
+    }
+  }
+})
+
+let News = sequelize.define('News', {});
+async function deleteRow() {
+  try {
+    await News.destroy({
+      where: {
+        created_at: {
+          // $lt: sevenDaysAgo,
+          [Op.lte]: sevenDaysAgo
+        },
+        // sportCategory: 'football'
+      },
+      // truncate: true,
+      // restartIdentity: true // works only with truncate
+    });
+    console.log('table cleared')
+  } catch (error) {
+    throw error
+  }
+  sequelize.close();
+}
+deleteRow();
